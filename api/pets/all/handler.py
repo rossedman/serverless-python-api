f858bc461fb5eb2f2a6cac2587b1ccb9382d35f6
Ross Edman <ross.edman@gmail.com>
2016-06-02 15:09:00 -0500

    Add retrieve all records

--- api/pets/all/handler.py
+++ api/pets/all/handler.py
@@ -3,16 +3,19 @@ from __future__ import print_function
 import boto3
 import json
 import logging
+from boto3.dynamodb.conditions import Key, Attr
+
+log = logging.getLogger()
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('pets')
 
 def handler(event, context):
-    
-    response = table.get_item(
-        Key={
-            'uuid': str(event["id"])
-        }
-    )
 
-    return response["Item"]
+    response = table.scan()
+
+    items = response['Items']
+
+    print(items)
+
+    return items
