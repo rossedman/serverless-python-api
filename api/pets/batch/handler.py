2ca922085c09dc44e245f51d7eb0afb1db731ad6
Ross Edman <ross.edman@gmail.com>
2016-06-02 16:57:30 -0500

    Batch Process Endpoint

---
+++ api/pets/batch/handler.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+
+import boto3
+import json
+import logging
+
+log = logging.getLogger()
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('pets')
+
+def handler(event, context):
+
+    users = event["body"]
+
+    for user in users:
+        table.put_item(
+            Item={
+                'uuid': str(user["uuid"]),
+                'gender': user["gender"],
+                'email': user["email"],
+                'ip_address': user["ip_address"],
+                'timezone': user["timezone"],
+                'first_name': user["first_name"],
+                'last_name': user["last_name"]
+            }
+        )
+
+    return {message: "Records added!"}
--- api/pets/create/handler.py
+++ api/pets/create/handler.py
@@ -11,14 +11,12 @@ table = dynamodb.Table('pets')
 def handler(event, context):
 
     unique_id = uuid.uuid4()
-    
-    payload = json.loads(event["body"])
 
     table.put_item(
         Item={
             'uuid': str(unique_id),
-            'name': payload["name"],
-            'breed': payload["breed"]
+            'name': event["body"]["name"],
+            'breed': event["body"]["breed"]
         }
     )
 
